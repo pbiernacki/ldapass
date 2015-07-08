a5b01393352189de8a44da24ef84465fc976f4fc
Paweł Biernacki <pawel.biernacki@gmail.com>
2015-07-08 15:12:02 +0100

    try different approach to travis

--- tests/openldap_install.sh
+++ tests/openldap_install.sh
@@ -3,9 +3,7 @@
 set -e
 set -x
 
-DEBIAN_FRONTEND=noninteractive sudo -E apt-get install -y --force-yes slapd time ldap-utils
-
-sudo slapadd -F /etc/ldap/slapd.d <<EOF
+slapadd -F /etc/ldap/slapd.d <<EOF
 dn: dc=example,dc=com
 objectClass: top
 objectClass: domain
